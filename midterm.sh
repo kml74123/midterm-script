b1df006e176a633096db2efcc6b0337db35edb75
kml74123 <35539960+kml74123@users.noreply.github.com>
2018-05-08 19:00:18 -0500

    Update midterm.sh

--- midterm.sh
+++ midterm.sh
@@ -3,34 +3,37 @@ echo 'this is my midterm script'
 
 echo 'check where we are'
 pwd
-echo 'create a dir call  kongmeng and a file call people'
-mkdir kongmeng
-touch people.txt
+echo 'create a dir call leeher and a file call people'
+mkdir leeher
+touch names.txt
 echo 'check if the file and dir is there'
 ls -l
-echo 'give permission to people.txt'
-chmod 777 people.txt
-echo 'write name into people.txt'
-echo 'kongmeng moua lee her lulu' >> people.txt
-echo 'use cat to see the names in people.txt'
-cat people.txt
-echo 'move the people.txt into kongmeng dir'
-mv  people.txt kongmeng
-echo 'check in kongmeng dir'
-cd kongmeng
+echo 'give permission to names.txt'
+chmod 777 names.txt
+echo 'write name into names.txt'
+echo 'kongmeng moua lee her lulu' >> names.txt
+echo 'use cat to see the names in names.txt'
+cat names.txt
+echo 'move the names.txt into leeher dir'
+mv  names.txt leeher
+echo 'check in leeher dir'
+cd leeher
 echo 'check with ls'
 ls -l
 echo 'yes the file is there'
 
-echo 'now create more files in kongmeng dir'
-touch ice fire water wind lighting
+echo 'now create more files in leeher dir'
+touch ice fire water wind lighting flower sky
 echo 'see if they are there'
 ls -l
 echo 'now delete all the file'
-rm ice fire water wind lighting people.txt
+rm ice fire water wind lighting names.txt flower sky
 echo 'check if they gone'
 ls -l
-
-
 echo 'it all gone'
+
+echo 'put leeher in a file'
+echo 'leeher' > leeherfile
+echo 'cat the leeherfile'
+cat leeherfile
 
